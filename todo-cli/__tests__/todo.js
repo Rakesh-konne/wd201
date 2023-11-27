953627608dd51fc2370fd59ad457194b0dfda886
unknown <ugs21051_cse.rakesh@cbit.org.in>
2023-11-27 16:22:20 +0530

    Add lint staged

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+
+const todoList = require("../todo");
+const { all, add, markAsComplete } = todoList();
+
+describe("Todolist Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+  test("should add new todo", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+
+  test("should mark a todo as complete ", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
