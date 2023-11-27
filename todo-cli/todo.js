1eb4729531adbb658ff6e623c9f826c769ec7bf3
Rakesh-konne <113461194+Rakesh-konne@users.noreply.github.com>
2023-11-27 13:36:55 +0530

    Updated in displayf'n todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -38,9 +38,11 @@ const todoList = () => {
     }
   
     const toDisplayableList = (list) => {
+          let result = "";
         for (let i = 0; i < list.length; i++) {
-           console.log(`[${list[i].completed === true ? "X" : " "}] ${list[i].title} ${list[i].dueDate===today? "": list[i].dueDate}`)
+            result +=`[${list[i].completed === true ? "X" : " "}] ${list[i].title} ${list[i].dueDate===today? "": list[i].dueDate}`+"\n"
         }
+        return result;
     }
   
     return {
