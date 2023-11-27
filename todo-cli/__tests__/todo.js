d6cf35e9f7ca170e1949ea67db095763fe59147c
unknown <ugs21051_cse.rakesh@cbit.org.in>
2023-11-27 16:43:29 +0530

    Add lint staged

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,14 +1,28 @@
 /* eslint-disable no-undef */
 
 const todoList = require("../todo");
-const { all, add, markAsComplete } = todoList();
+const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
 
 describe("Todolist Test Suite", () => {
   beforeAll(() => {
     add({
-      title: "Test todo",
+      title: "Overdue Todo",
+      completed: false,
+      dueDate: new Date(
+        new Date().setDate(new Date().getDate() - 1),
+      ).toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "dueToday Todo",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "dueLater Todo",
+      completed: false,
+      dueDate: new Date(
+        new Date().setDate(new Date().getDate() + 1),
+      ).toLocaleDateString("en-CA"),
     });
   });
   test("should add new todo", () => {
@@ -26,5 +40,23 @@ describe("Todolist Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("should retrieve overdue todos", () => {
+    const overdueTodos = overdue();
+    expect(overdueTodos.length).toBe(1);
+    expect(overdueTodos[0].title).toBe("Overdue Todo");
+  });
+
+  test("should retrieve dueToday todos", () => {
+    const overdueTodos = dueToday();
+    expect(overdueTodos.length).toBe(2);
+    expect(overdueTodos[0].title).toBe("dueToday Todo");
+  });
+
+  test("should retrieve  dueLater todos", () => {
+    const overdueTodos = dueLater();
+    expect(overdueTodos.length).toBe(1);
+    expect(overdueTodos[0].title).toBe("dueLater Todo");
   });
 });
